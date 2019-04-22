8be6d9a267c1acb6dfb875b4744a7f605b4df11f
Ya-Liang Chang (Allen) <b03901014@ntu.edu.tw>
2019-04-22 12:49:59 +0800

    Fix indentation

--- src/main/java/tw/edu/ntu/lowerbound10hours/jlask/Principal.java
+++ src/main/java/tw/edu/ntu/lowerbound10hours/jlask/Principal.java
@@ -1,23 +1,20 @@
-package tw.edu.ntu.lowerbound10hours.java; 
+package tw.edu.ntu.lowerbound10hours.java;
 
-import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
-
+import org.apache.logging.log4j.Logger;
 
 public class Principal {
 
-    private static final Logger LOGGER = LogManager.getLogger(Principal.class.getName());
+  private static final Logger LOGGER = LogManager.getLogger(Principal.class.getName());
 
-    private Principal() {
+  private Principal() {}
 
-    }
-
-    public static void main(String[] args) {
-        LOGGER.trace("begin of program"); // only for test
-        Point p = new Point(3, 4);
-        LOGGER.info(p.toString()); // a message for the user
-        LOGGER.warn("a fake warning");
-        LOGGER.error("a fake error");
-        LOGGER.trace("end of program"); // only for test
-    }
+  public static void main(String[] args) {
+    LOGGER.trace("begin of program"); // only for test
+    Point p = new Point(3, 4);
+    LOGGER.info(p.toString()); // a message for the user
+    LOGGER.warn("a fake warning");
+    LOGGER.error("a fake error");
+    LOGGER.trace("end of program"); // only for test
+  }
 }
