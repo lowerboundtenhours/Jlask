84d76c6944834a259caecc23e90914a9bfe74097
Ya-Liang Chang (Allen) <b03901014@ntu.edu.tw>
2019-04-22 12:35:06 +0800

    Fix java pacakage path

--- src/test/java/tw/edu/ntu/lowerbound10hours/jlask/PrincipalTest.java
+++ src/test/java/tw/edu/ntu/lowerbound10hours/jlask/PrincipalTest.java
@@ -1,23 +1,7 @@
-package fr.paris10.pascalpoizat.templates.javaproject;
+package tw.edu.ntu.lowerbound10hours.java;   
 
 import org.testng.annotations.Test;
 
-/**
- * template-java-project
- * Copyright 2015 pascalpoizat
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 public class PrincipalTest {
 
