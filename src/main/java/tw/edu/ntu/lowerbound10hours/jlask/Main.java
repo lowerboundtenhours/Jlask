c06e6e7a2ae1e9efaf97c5d6cb7da915addff414
Ya-Liang Chang (Allen) <b03901014@ntu.edu.tw>
2019-05-10 22:51:04 +0800

    Add Main.java in jlask

---
+++ src/main/java/tw/edu/ntu/lowerbound10hours/jlask/Main.java
@@ -0,0 +1,15 @@
+package tw.edu.ntu.lowerbound10hours.jlask;
+
+import java.net.InetAddress;
+import tw.edu.ntu.lowerbound10hours.jerkzeug.Application;
+import tw.edu.ntu.lowerbound10hours.jerkzeug.TestApplication;
+import tw.edu.ntu.lowerbound10hours.jerkzeug.serving.Serving;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    InetAddress host = InetAddress.getByName("127.0.0.1");
+    int port = 8013;
+    Application testApp = new TestApplication();
+    Serving.runSimple(host, port, testApp);
+  }
+}
