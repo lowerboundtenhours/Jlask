2b0e0c7bc13a93cc42e9e9676a5ba32511f84e28
劉哲宇 <j2325138@gmail.com>
2019-05-16 22:26:23 +0800

    Add more test; Run goJF

--- src/test/java/tw/edu/ntu/lowerbound10hours/jerkzeug/routing/MapTest.java
+++ src/test/java/tw/edu/ntu/lowerbound10hours/jerkzeug/routing/MapTest.java
@@ -1,9 +1,12 @@
 package tw.edu.ntu.lowerbound10hours.jerkzeug.routing;
 
+import static org.testng.Assert.assertEquals;
+
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
-
+import java.util.HashMap;
+import javafx.util.Pair;
 import manifold.ext.api.Jailbreak;
 import org.eclipse.jetty.server.Server;
 import org.testng.annotations.Test;
@@ -13,27 +16,29 @@ import tw.edu.ntu.lowerbound10hours.jerkzeug.serving.WsgiRequestHandler;
 
 public class MapTest {
 
-    private Map exampleMap() {
+  public Map exampleMap() {
     ArrayList<RuleFactory> rules = new ArrayList<>();
     rules.add(new Rule("/", "index"));
     rules.add(new Rule("/downloads/", "downloads/index"));
     rules.add(new Rule("/downloads/<int:id>", "downloads/show"));
     return new Map(rules);
+  }
+
+  public java.util.Map<String, Object> exampleEnvironment() {
+    int port = 8001;
+    String name = "localhost";
+    try {
+      InetAddress host = InetAddress.getByName(name);
+      Application testApp = new TestApplication();
+      Server testServer = new Server(new InetSocketAddress(host, port));
+      @Jailbreak WsgiRequestHandler handler = new WsgiRequestHandler(testApp, testServer);
+      java.util.Map<String, Object> environ = handler.makeEnviron(null, null, null, null);
+      return environ;
+    } catch (Exception e) {
+      return null;
     }
-    private java.util.Map<String, Object> exampleEnvironment() {
-      int port = 8001;
-      String name = "localhost";
-      try {
-          InetAddress host = InetAddress.getByName(name);
-          Application testApp = new TestApplication();
-          Server testServer = new Server(new InetSocketAddress(host, port));
-          @Jailbreak WsgiRequestHandler handler = new WsgiRequestHandler(testApp, testServer);
-          java.util.Map<String, Object> environ = handler.makeEnviron(null, null, null, null);
-          return environ;
-      } catch (Exception e) {
-          return null;
-      }
-    } 
+  }
+
   @Test
   public void testMap() {
     Map map = exampleMap();
@@ -41,8 +46,14 @@ public class MapTest {
 
   @Test
   public void testBindToEnviron() {
-      Map map = exampleMap();
-      java.util.Map<String, Object> environ = exampleEnvironment();
-      map.bindToEnvironment(environ, null, null);
+    Map map = exampleMap();
+    java.util.Map<String, Object> environ = exampleEnvironment();
+    MapAdapter urls = map.bindToEnvironment(environ, null, null);
+
+    Pair<String, HashMap<String, Object>> result;
+    result = urls.match("/downloads/12");
+    assertEquals(result.getKey(), "downloads/show");
+    assertEquals(result.getValue().size(), 1);
+    assertEquals((Integer) result.getValue().get("id"), new Integer(12));
   }
 }
