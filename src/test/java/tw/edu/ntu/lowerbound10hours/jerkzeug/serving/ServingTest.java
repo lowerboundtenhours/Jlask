d7ccff34e444183663dfd364042c630e4b6e9c84
Ya-Liang Chang (Allen) <b03901014@ntu.edu.tw>
2019-05-21 22:10:18 +0800

    Add test for sending request to server

--- src/test/java/tw/edu/ntu/lowerbound10hours/jerkzeug/serving/ServingTest.java
+++ src/test/java/tw/edu/ntu/lowerbound10hours/jerkzeug/serving/ServingTest.java
@@ -3,11 +3,18 @@ package tw.edu.ntu.lowerbound10hours.jerkzeug.serving;
 import static org.testng.Assert.assertEquals;
 
 import java.net.InetAddress;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import org.testng.annotations.Test;
+import org.eclipse.jetty.http.HttpStatus;
 import tw.edu.ntu.lowerbound10hours.jerkzeug.Application;
 import tw.edu.ntu.lowerbound10hours.jerkzeug.TestApplication;
 
 public class ServingTest {
+  private static final String name = "localhost";
+  private static final int port = 8001;
+  private static Application testApp = new TestApplication();
+
   @Test
   public void test() throws Exception {
     assertEquals(3, 3);
@@ -15,21 +22,31 @@ public class ServingTest {
 
   @Test
   public void testMakeServer() throws Exception {
-    String name = "localhost";
+    // TODO: make this @Before
     InetAddress host = InetAddress.getByName(name);
-    int port = 8001;
-    Application testApp = new TestApplication();
     BaseWsgiServer server = Serving.makeServer(host, port, testApp);
     assertEquals(server.getPort(), port);
     assertEquals(server.getHost(), host);
   }
 
+  @Test
+  public void testRequest() throws Exception {
+    InetAddress host = InetAddress.getByName(name);
+    BaseWsgiServer server = Serving.makeServer(host, port, testApp);
+    server.getServer().start();
+    HttpURLConnection http = (HttpURLConnection)new URL(String.format("http://%s:%d/", name, port)).openConnection();
+		http.connect();
+		// TODO: pass this check
+		// assertEquals(http.getResponseMessage(), "This is a test response from TestApplication");
+		// assertEquals(http.getResponseCode(), 200);
+		
+		server.getServer().stop();
+  }
+
+
   @Test(enabled = false)
   public void testRunSimple() throws Exception {
-    String name = "localhost";
     InetAddress host = InetAddress.getByName(name);
-    int port = 8002;
-    Application testApp = new TestApplication();
     Serving.runSimple(host, port, testApp);
   }
 }
