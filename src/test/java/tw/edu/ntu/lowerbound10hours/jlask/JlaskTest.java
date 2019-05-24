630194880d2e77fb1e632ffa7886416582274fd1
Ya-Liang Chang (Allen) <b03901014@ntu.edu.tw>
2019-05-24 18:30:34 +0800

    Rename jlask in JlaskTest to testApp

--- src/test/java/tw/edu/ntu/lowerbound10hours/jlask/JlaskTest.java
+++ src/test/java/tw/edu/ntu/lowerbound10hours/jlask/JlaskTest.java
@@ -30,7 +30,7 @@ public class JlaskTest {
 
   @Test
   public void testMakeResponse() throws Exception {
-    @Jailbreak Jlask jlask = new Jlask();
+    @Jailbreak Jlask testApp = new Jlask();
     String rv = "foo";
     @Jailbreak Response res = jlask.make_response(rv);
     assertEquals(res.response.get(0), rv);
@@ -38,22 +38,22 @@ public class JlaskTest {
 
   @Test
   public void testAddUrlRule() throws Exception {
-    @Jailbreak Jlask jlask = new Jlask();
-    assertEquals(jlask.viewFunctions.containsKey("index"), false);
-    jlask.addUrlRule("/", "index", new MyView());
-    jlask.addUrlRule("/login", "login", new MyView());
-    assertEquals(jlask.viewFunctions.containsKey("login"), true);
-    assertEquals(jlask.viewFunctions.containsKey("index"), true);
+    @Jailbreak Jlask testApp = new Jlask();
+    assertEquals(testApp.viewFunctions.containsKey("index"), false);
+    testApp.addUrlRule("/", "index", new MyView());
+    testApp.addUrlRule("/login", "login", new MyView());
+    assertEquals(testApp.viewFunctions.containsKey("login"), true);
+    assertEquals(testApp.viewFunctions.containsKey("index"), true);
   }
 
   private Jlask buildApp() {
-    Jlask app = new Jlask();
-    app.addUrlRule("/", "index", new MyView());
+    Jlask testApp = new Jlask();
+    testApp.addUrlRule("/", "index", new MyView());
     return app;
   }
 
   private Jlask buildAppAndSendReuqest() {
-    @Jailbreak Jlask jlask = buildApp();
+    @Jailbreak Jlask testApp = buildApp();
     Map<String, Object> environ = new HashMap<String, Object>();
     environ.put("baseRequest", mockedRequest);
     RequestContext ctx = new RequestContext(jlask, environ);
@@ -63,8 +63,8 @@ public class JlaskTest {
 
   @Test()
   public void testDispatchRequest() throws Exception {
-    @Jailbreak Jlask jlask = this.buildAppAndSendReuqest();
-    String r = jlask.dispatchRequest();
+    @Jailbreak Jlask testApp = this.buildAppAndSendReuqest();
+    String r = testApp.dispatchRequest();
     assertEquals(r, "foo");
   }
 
@@ -84,44 +84,52 @@ public class JlaskTest {
 
   @Test
   public void testHandleException() throws Exception {
-    @Jailbreak Jlask jlask = buildApp();
+    @Jailbreak Jlask testApp = buildApp();
     // For now handleException() will only throw InternalServerError
-    assertThrows(InternalServerError.class, () -> jlask.handleException(new Exception()));
+    assertThrows(InternalServerError.class, () -> testApp.handleException(new Exception()));
   }
 
   @Test
   public void testHandleUserException() throws Exception {
-    @Jailbreak Jlask jlask = buildApp();
+    @Jailbreak Jlask testApp = buildApp();
     // For now handleUserException() will only throw whatever comes in back if it is not a
     // HttpException. Otherwise, it will return HttpException
-    assertThrows(Exception.class, () -> jlask.handleUserException(new Exception()));
+    assertThrows(Exception.class, () -> testApp.handleUserException(new Exception()));
     InternalServerError e = new InternalServerError();
-    assertEquals(e, jlask.handleUserException(e));
+    assertEquals(e, testApp.handleUserException(e));
   }
 
   @Test
   public void testHttpException() throws Exception {
-    @Jailbreak Jlask jlask = buildApp();
+    @Jailbreak Jlask testApp = buildApp();
     // For now handleException() will only return given HttpException
     InternalServerError e = new InternalServerError();
-    assertEquals(e, jlask.handleHttpException(e));
+    assertEquals(e, testApp.handleHttpException(e));
   }
 
   @Test
   public void testFindErrorHandler() {
-    @Jailbreak Jlask jlask = buildApp();
+    @Jailbreak Jlask testApp = buildApp();
     // For now there is no error handler
     InternalServerError e = new InternalServerError();
-    assertEquals(null, jlask.findErrorHandler(e));
+    assertEquals(null, testApp.findErrorHandler(e));
   }
 
-  @Test()
-  public void testNotFound() throws Exception {
-    @Jailbreak Jlask jlask = this.buildAppAndSendReuqest();
+  @Test
+  public void testWsgiApp() throws Exception {
+    @Jailbreak Jlask testApp = this.buildAppAndSendReuqest();
     
     Map<String, Object> environ = new HashMap<String, Object>();
     StartResponse startResponse = mock(StartResponse.class);
-    jlask.wsgiApp(environ, startResponse);
+    testApp.wsgiApp(environ, startResponse);
+  }
+
+  @Test
+  public void testNotFound() {
+    @Jailbreak Jlask testApp = this.buildAppAndSendReuqest();
+    testApp.addUrlRule("/", "index", new MyView());
+    testApp.addUrlRule("/login", "login", new MyView());
+    BaseWsgiServer server = Serving.makeServer(host, port, app);
   }
 
 }
