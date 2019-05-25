dacc611e2de17f29afdb03481bec638b56e4bfa4
vic85821 <vic85821@gmail.com>
2019-05-25 12:07:29 +0800

    User hibernate to connect and perform operations to SQLite

--- src/main/java/tw/edu/ntu/lowerbound10hours/jlask/BlogGlobal.java
+++ src/main/java/tw/edu/ntu/lowerbound10hours/jlask/BlogGlobal.java
@@ -2,25 +2,28 @@ package tw.edu.ntu.lowerbound10hours.jlask;
 
 import java.util.HashMap;
 import java.util.Map;
+import tw.edu.ntu.lowerbound10hours.jlaskhibernate.User;
 
 public class BlogGlobal {
-  public static Map<String, Object> getDatabase() {
-    return database;
-  }
 
   public static Map<String, String> getLoginUser() {
     return loginUser;
   }
 
-  public static void setLoginUser(Map<String, String> user) {
-    loginUser = user;
+  public static void setLoginUser(User user) {
+    Map<String, String> _user = new HashMap<String, String>();
+    if (user != null) {
+      _user.put("username", user.getUsername());
+      _user.put("password", user.getPassword());
+      _user.put("id", String.valueOf(user.getId()));
+    }
+    loginUser = _user;
   }
 
-  public static int getPostId(){
+  public static int getPostId() {
     return postId++;
   }
 
-  private static Map<String, Object> database = new HashMap<String, Object>();
   private static Map<String, String> loginUser = new HashMap<String, String>();
   private static int postId = 0;
 }
--- src/main/java/tw/edu/ntu/lowerbound10hours/jlask/wrappers/Request.java
+++ src/main/java/tw/edu/ntu/lowerbound10hours/jlask/wrappers/Request.java
@@ -1,23 +1,14 @@
 package tw.edu.ntu.lowerbound10hours.jlask.wrappers;
 
 import java.util.Map;
-import tw.edu.ntu.lowerbound10hours.jerkzeug.routing.Rule;
 
-// Extend werkzeug.wrappers.Request
-public class Request {
-  public Map<String, Object> environ;
-  public Rule rule;
-  public Map<String, Object> viewArgs;
-
+public class Request extends tw.edu.ntu.lowerbound10hours.jerkzeug.wrappers.Request {
+  /**
+   * Extends jerkjeug.wrappers.Request.
+   *
+   * @param environ WSGI environ
+   */
   public Request(Map<String, Object> environ) {
-    this.environ = environ;
+    super(environ);
   }
-
-  // public Map<String, Object> getEnv() {
-  //   return this.environ;
-  // }
-
-  // public Rule getRule() {
-  //   return this.rule;
-  // }
 }
