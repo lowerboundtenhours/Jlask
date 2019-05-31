ecd6b8c20bcfb6a7e30a1ac51595162b9dac2767
tall15421542 <b04703001@ntu.edu.tw>
2019-05-31 23:24:11 +0800

    back to 69.114

--- src/test/java/tw/edu/ntu/lowerbound10hours/example/DeleteViewTest.java
+++ src/test/java/tw/edu/ntu/lowerbound10hours/example/DeleteViewTest.java
@@ -6,10 +6,8 @@ import java.net.URL;
 import org.testng.annotations.Test;
 import tw.edu.ntu.lowerbound10hours.jerkzeug.serving.BaseWsgiServer;
 import tw.edu.ntu.lowerbound10hours.jerkzeug.serving.Serving;
-import tw.edu.ntu.lowerbound10hours.jlask.Global;
 import tw.edu.ntu.lowerbound10hours.jlask.Jlask;
 import tw.edu.ntu.lowerbound10hours.jlask.TemplateEngine;
-import tw.edu.ntu.lowerbound10hours.jlask.session.SecureCookieSession;
 import tw.edu.ntu.lowerbound10hours.jlaskhibernate.Hibernate;
 
 public class DeleteViewTest {
@@ -29,9 +27,6 @@ public class DeleteViewTest {
     InetAddress host = InetAddress.getByName(name);
     BaseWsgiServer server = Serving.makeServer(host, port, application);
     server.getServer().start();
-    SecureCookieSession session = (SecureCookieSession) Global.session();
-    session.set("username", "t");
-    session.set("id", "1");
     HttpURLConnection http =
         (HttpURLConnection) new URL(String.format("http://%s:%d/1", name, port)).openConnection();
     http.setRequestMethod("GET");
@@ -61,9 +56,6 @@ public class DeleteViewTest {
     InetAddress host = InetAddress.getByName(name);
     BaseWsgiServer server = Serving.makeServer(host, port, application);
     server.getServer().start();
-    SecureCookieSession session = (SecureCookieSession) Global.session();
-    session.set("username", "t");
-    session.set("id", "1");
     HttpURLConnection http =
         (HttpURLConnection) new URL(String.format("http://%s:%d/1", name, port)).openConnection();
     http.setRequestMethod("POST");
